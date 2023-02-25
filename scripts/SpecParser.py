6a3f90198afe2192c125275c470ea3b1e6c9b0c0
xaz <1300019770@qq.com>
2023-02-25 20:13:35 +0800

    Still Have Problems with form recognizer

--- scripts/SpecParser.py
+++ scripts/SpecParser.py
@@ -4,6 +4,7 @@ import PyPDF2
 from pdf2image import convert_from_path
 import cv2
 import os
+import re
 
 MAX_LINE_BREAK = 25
 MIN_LINE_LENGTH = 30
@@ -11,7 +12,7 @@ MAX_LINE_GRAY = 20
 MAX_GRAY_GAP = 25
 PAGE_MARGIN_LEFT = 246
 PAGE_MARGIN_RIGHT = 1403
-CELL_MARGIN_WIDTH = 3
+CELL_MARGIN_WIDTH = 2
 
 
 def clean_text(text):
@@ -50,6 +51,7 @@ class PDFFormRecognizer:
     def  __init__(self, path, start, end) -> None:
         self.images = convert_from_path('Core_v5.3.pdf', fmt="jpeg", first_page=start, last_page=end)
         self.cells = 0
+        self.page = start
 
     def locate_vertical(self, arr):
         indices = np.flatnonzero(np.concatenate(([True], arr[1:]!=arr[:-1],[True])))
@@ -70,51 +72,73 @@ class PDFFormRecognizer:
                 cur += 1
         return real_lines
     
-    def shrink_rect(self, x1, x2, y1, y2):
+    def shrink_rect(self, x1, x2, y1, y2, w):
         x_min = min(x1, x2)
         x_max = max(x1, x2)
         y_min = min(y1, y2)
         y_max = max(y1, y2)
-        return x_min + CELL_MARGIN_WIDTH, x_max - CELL_MARGIN_WIDTH, y_min + CELL_MARGIN_WIDTH, y_max - CELL_MARGIN_WIDTH
+        return x_min + w, x_max - w, y_min + w, y_max - w
 
     def image_to_string(self, image):
+        print(self.page)
         cv2.imwrite("/tmp/tmp.png", image)
         os.system("tesseract /tmp/tmp.png /tmp/res 2> /dev/null")
         with open('/tmp/res.txt', 'r') as f:
             text = f.read()
         os.system('rm /tmp/res.txt')
         return text
-
-    def parse_command(self, X, Y):
-        cell1 = self.shrink_rect(X[0], X[1], Y[1], Y[2])
-        cell1_text = self.image_to_string(self.gray[cell1[2]:cell1[3],cell1[0]: cell1[1]])
-        cell2 = self.shrink_rect(X[1], X[2], Y[1], Y[2])
-        cell2_text = self.image_to_string(self.gray[cell2[2]:cell2[3],cell2[0]: cell2[1]])
-        cell3 = self.shrink_rect(X[2], X[3], Y[1], Y[2])
-        cell3_text = self.image_to_string(self.gray[cell3[2]:cell3[3],cell3[0]: cell3[1]])
-        cell4 = self.shrink_rect(X[3], X[4], Y[1], Y[2])
-        cell4_text = self.image_to_string(self.gray[cell4[2]:cell4[3],cell4[0]: cell4[1]])
-        cmd = cell1_text.replace('-','').replace('\n','').strip()
-        ocf = eval(cell2_text.lower().replace('o','0'))
-        params = [ param.replace('\n','').replace(' ','') for param in cell3_text.split(',')]
-        ret_params = [ param.replace('\n','').replace(' ','') for param in cell4_text.split(',')]
-        print((cmd, ocf, params, ret_params))
-
-    def locate_cells(self, start, end):
-        x1, x2, y1, y2 = self.shrink_rect(PAGE_MARGIN_LEFT, PAGE_MARGIN_RIGHT, start, end)
-        X , Y= [], []
+    
+    def parse_cells(self, x_start, x_end, y_start, y_end):
+        cells = []
+        X, Y = [], []
+        s = 1
+        x1, x2, y1, y2 = self.shrink_rect(x_start, x_end, y_start, y_end, 3)
+        for y in range(y1, y2):
+            if s*(self.gray[y, x1] - self.gray[y+1, x1]) > MAX_GRAY_GAP:
+                Y.append(y+1)
+                s = -s
         for x in range(x1, x2):
             if self.gray[y1, x] - self.gray[y1, x+1] > MAX_GRAY_GAP:
+                X.append(x+1)
+        X = [x_start] + X + [x_end]
+        Y = [y_start] + Y + [y_end]
+
+        for i in range(len(Y)-1):
+            cells.append([])
+            y1, y2 = Y[i], Y[i+1]
+            for j in range(len(X)-1):
+                x1, x2 = X[j], X[j+1]
+                x1, x2, y1, y2 = self.shrink_rect(x1, x2, y1, y2, 3)
+                print((x1, x2, y1, y2))
+                image = self.image[y1:y2,x1:x2]
+                cells[-1].append(self.image_to_string(image))
+        return cells
+
+    def parse_command(self, X, Y):
+        cells = self.parse_cells(X[0], X[1], Y[0], Y[1])
+        for row in cells:
+            cmd = row[0].replace('-','').replace('\n','').strip()
+            ocf = eval(row[1].lower().replace('o','0'))
+            params = [ re.sub('[\n\-\s]', '', param) for param in row[2].split(',')]
+            ret_params = [ re.sub('[\n\-\s]', '', param) for param in row[3].split(',')]
+            print((cmd, ocf, params, ret_params))
+
+    def parse_form(self, start, end):
+        x1, x2, y1, y2 = self.shrink_rect(PAGE_MARGIN_LEFT, PAGE_MARGIN_RIGHT, start, end, 2)
+        X , Y= [PAGE_MARGIN_LEFT], [start]
+        for x in range(PAGE_MARGIN_LEFT, PAGE_MARGIN_RIGHT):
+            if self.gray[y1, x] - self.gray[y1, x+1] > MAX_GRAY_GAP:
                 X.append(x)
-        for y in range(y1, y2):
+                break
+        for y in range(start, end):
             if self.gray[y, x1] - self.gray[y+1, x1] > MAX_GRAY_GAP:
                 Y.append(y)
-        X = [PAGE_MARGIN_LEFT] + X + [PAGE_MARGIN_RIGHT]
-        Y = [start] + Y + [end]
-
-        x1, x2, y1, y2 = self.shrink_rect(X[0], X[1], Y[0], Y[1])
+                break
+        print(X, Y)
+        x1, x2, y1, y2 = self.shrink_rect(X[0], X[1], Y[0], Y[1], 3)
         gray = self.gray[y1:y2,x1:x2]
         text = self.image_to_string(gray)
+        X, Y = [PAGE_MARGIN_LEFT, PAGE_MARGIN_RIGHT], [Y[1], end]
         if text.strip() == 'Command':
             self.parse_command(X, Y)
         elif text.strip() == 'Value':
@@ -135,16 +159,16 @@ class PDFFormRecognizer:
 
     def analyze(self):
         for image in self.images:
-            self.image = image
-            pixels = np.array(image)
-            self.gray = self.rgb2gray(pixels)
+            self.image = np.array(image)
+            self.gray = self.rgb2gray(self.image)
             col = self.gray[:, PAGE_MARGIN_LEFT]
             vertical_lines = self.locate_vertical(col)
             for line in vertical_lines:
-                self.locate_cells(line[0], line[1])
+                self.parse_form(line[0], line[1])
+            self.page += 1
 
 
 
 if __name__ == "__main__":
     # text_extractor("Core_v5.3.pdf", 1846, 1848)
-    PDFFormRecognizer("Core_v5.3.pdf", 1846, 1949).analyze()
+    PDFFormRecognizer("Core_v5.3.pdf", 1846, 2164).analyze()
